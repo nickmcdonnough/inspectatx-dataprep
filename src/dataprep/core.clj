d954204b8e33cc0521d6fe375e2959a3fa6ccf3c
Nick McDonnough <nickmcd@gmail.com>
2015-04-12 16:04:47 -0500

    Stops keyifying the column names of records with mappify. Having strings as field names in the record maps makes the access of those fields a lot cleaner.

--- src/dataprep/core.clj
+++ src/dataprep/core.clj
@@ -17,7 +17,7 @@
   "The purpose of this function is to verify that lat long is clean."
   (s/replace angle #"[^0-9\.\-]" ""))
 
-(defn split-point [address]
+(defn address->address+latlong [address]
   "Takes the :Address value from a record and splits it up. Returns a map
    containing the address and two new latitude and longitude fields."
   (let [[addr-part lat-part] (s/split address #"\n\(")
@@ -27,23 +27,23 @@
      :longitude (clean-latlon long)}))
 
 (defn record->restaurant [record]
-  (merge (split-point (:Address record))
-         {:facility_id (get record (keyword "Facility ID"))
-          :name        (get record (keyword "Restaurant Name"))
-          :zip         (get record (keyword "Zip Code"))}))
+  (merge (address->address+latlong (get record "Address"))
+         {:facility_id (get record "Facility ID")
+          :name        (get record "Restaurant Name")
+          :zip         (get record "Zip Code")}))
 
 (defn record->inspection [record]
-  {:date        (get record (keyword "Inspection Date"))
-   :score       (get record :Score)
-   :facility_id (get record (keyword "Facility ID"))
-   :description (get record (keyword "Process Description"))})
+  {:date        (get record "Inspection Date")
+   :score       (get record "Score")
+   :facility_id (get record "Facility ID")
+   :description (get record "Process Description")})
 
 (defn read-input-csv [filename]
   (with-open [in-file (io/reader filename)]
     (doall
      (->> (csv/read-csv in-file)
           (sc/remove-comments)
-          (sc/mappify)))))
+          (sc/mappify {:keyify false})))))
 
 (defn write-csv [filename dataset]
   (with-open [writer (io/writer filename)]
